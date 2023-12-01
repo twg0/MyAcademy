d0b79cb89eb303080b67f059382411a3cd70bbba
twg0 <fruitsjh@gmail.com>
2023-12-01 16:58:12 +0900

    feat: Exam entity 에 BaseEntity 설정

--- src/main/java/com/twg0/myacademy/domain/exam/entity/Exam.java
+++ src/main/java/com/twg0/myacademy/domain/exam/entity/Exam.java
@@ -2,6 +2,8 @@ package com.twg0.myacademy.domain.exam.entity;
 
 import java.time.Instant;
 
+import com.twg0.myacademy.domain.common.entity.BaseEntity;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Exam {
+public class Exam extends BaseEntity {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 

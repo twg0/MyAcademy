b3dfd309319e04d2ed59bfbbbd6d80b94d12dbb3
twg0 <fruitsjh@gmail.com>
2023-12-01 15:18:19 +0900

    feat: Academy entity 작성

---
+++ src/main/java/com/twg0/myacademy/domain/academy/entity/Academy.java
@@ -0,0 +1,31 @@
+package com.twg0.myacademy.domain.academy.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Academy {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String name;
+	private String phoneNumber;
+	private String address;
+	private Integer studentNumber;
+
+	@Builder
+	public Academy(String name, String phoneNumber, String address, Integer studentNumber) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+		this.studentNumber = studentNumber;
+	}
+
+	/* 연관관계 필요 */
+}

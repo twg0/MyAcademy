6121ff01513a4d9d612cbe81573eeee4df9e032a
twg0 <fruitsjh@gmail.com>
2023-12-01 16:58:47 +0900

    fix: Grade Entity 위치 변경 - 테스트 하위

--- src/main/java/com/twg0/myacademy/domain/exam/entity/Grade.java
+++ src/main/java/com/twg0/myacademy/domain/exam/entity/Grade.java
@@ -1,4 +1,6 @@
-package com.twg0.myacademy.domain.grade.entity;
+package com.twg0.myacademy.domain.exam.entity;
+
+import com.twg0.myacademy.domain.common.entity.BaseEntity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Grade {
+public class Grade extends BaseEntity {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 

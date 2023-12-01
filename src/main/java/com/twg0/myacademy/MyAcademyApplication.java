14025c41962a37a3ae15cebff4c8a9cc404e4145
twg0 <fruitsjh@gmail.com>
2023-12-01 15:32:32 +0900

    feat: JpaAuditing 설정

--- src/main/java/com/twg0/myacademy/MyAcademyApplication.java
+++ src/main/java/com/twg0/myacademy/MyAcademyApplication.java
@@ -2,7 +2,9 @@ package com.twg0.myacademy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MyAcademyApplication {
 

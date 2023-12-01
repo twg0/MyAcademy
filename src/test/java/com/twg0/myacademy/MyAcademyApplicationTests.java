2960a59fecc2de68238e4b7e5a27d1c2ab989c14
twg0 <fruitsjh@gmail.com>
2023-12-01 11:37:11 +0900

    initial commits

---
+++ src/test/java/com/twg0/myacademy/MyAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twg0.myacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

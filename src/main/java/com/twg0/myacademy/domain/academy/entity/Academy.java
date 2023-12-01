3c47723a44d28686082a83970947e7497560627a
twg0 <fruitsjh@gmail.com>
2023-12-01 15:33:38 +0900

    feat: BaseEntity 상속 및 Getter 설정

--- src/main/java/com/twg0/myacademy/domain/academy/entity/Academy.java
+++ src/main/java/com/twg0/myacademy/domain/academy/entity/Academy.java
@@ -1,16 +1,20 @@
 package com.twg0.myacademy.domain.academy.entity;
 
+import com.twg0.myacademy.domain.common.entity.BaseEntity;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Academy {
+public class Academy extends BaseEntity {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 

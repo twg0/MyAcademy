e8ed1c3f386ee00c7de2f6b7f0a5b4ea6f861162
twg0 <fruitsjh@gmail.com>
2023-12-02 18:11:09 +0900

    feat: Classes 연관관계 설정

--- src/main/java/com/twg0/myacademy/domain/classes/entity/Classes.java
+++ src/main/java/com/twg0/myacademy/domain/classes/entity/Classes.java
@@ -1,11 +1,16 @@
 package com.twg0.myacademy.domain.classes.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.twg0.myacademy.domain.common.entity.BaseEntity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,6 +21,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Classes extends BaseEntity {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "classes_id")
 	private Long id;
 
 	private String subject;
@@ -30,4 +36,14 @@ public class Classes extends BaseEntity {
 	}
 
 	/* 연관관계 설정 */
+	@OneToMany(mappedBy = "classes")
+	private List<MemberClasses> memberClasses = new ArrayList<>();
+
+	public void addMemberClasses(MemberClasses memberClasses) {
+		this.memberClasses.add(memberClasses);
+	}
+
+	public void removeMemberClasses(MemberClasses memberClasses) {
+		this.memberClasses.remove(memberClasses);
+	}
 }

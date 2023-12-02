4991291417def17374a92dc40bb5997387892820
twg0 <fruitsjh@gmail.com>
2023-12-02 18:12:03 +0900

    feat: Member 연관관계 설정

--- src/main/java/com/twg0/myacademy/domain/member/entity/Member.java
+++ src/main/java/com/twg0/myacademy/domain/member/entity/Member.java
@@ -1,14 +1,24 @@
 package com.twg0.myacademy.domain.member.entity;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.twg0.myacademy.domain.academy.entity.Academy;
+import com.twg0.myacademy.domain.classes.entity.Classes;
+import com.twg0.myacademy.domain.classes.entity.MemberClasses;
 import com.twg0.myacademy.domain.common.entity.BaseEntity;
+import com.twg0.myacademy.domain.exam.entity.Grade;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,7 +28,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseEntity {
-	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "member_id")
 	private Long id;
 
 	@Column(unique = true)
@@ -29,12 +41,39 @@ public class Member extends BaseEntity {
 	private Integer age;
 
 	@Builder
-	public Member(String username, Instant birth, String school, Integer age) {
+	public Member(String username, Instant birth, String school, Integer age, Academy academy) {
 		this.username = username;
 		this.birth = birth;
 		this.school = school;
 		this.age = age;
+		this.academy = academy;
 	}
 
+
 	/* 연관관계 필요 */
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "academy_id")
+	private Academy academy;
+
+	@OneToMany(mappedBy = "member")
+	private List<MemberClasses> memberClasses = new ArrayList<>();
+
+	@OneToMany(mappedBy = "member")
+	private List<Grade> grades = new ArrayList<>();
+
+	public void addMemberClasses(MemberClasses memberClasses) {
+		this.memberClasses.add(memberClasses);
+	}
+
+	public void removeMemberClasses(MemberClasses memberClasses) {
+		this.memberClasses.remove(memberClasses);
+	}
+
+	public void addGrades(Grade grade) {
+		this.grades.add(grade);
+	}
+
+	public void removeGrades(Grade grade) {
+		this.grades.remove(grade);
+	}
 }

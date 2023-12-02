bf124b4e17da23db174d1ddb6482c1411351bbf9
twg0 <fruitsjh@gmail.com>
2023-12-02 18:11:55 +0900

    feat: Grade 연관관계 설정

--- src/main/java/com/twg0/myacademy/domain/exam/entity/Grade.java
+++ src/main/java/com/twg0/myacademy/domain/exam/entity/Grade.java
@@ -1,13 +1,19 @@
 package com.twg0.myacademy.domain.exam.entity;
 
 import com.twg0.myacademy.domain.common.entity.BaseEntity;
+import com.twg0.myacademy.domain.member.entity.Member;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -26,4 +32,18 @@ public class Grade extends BaseEntity {
 	}
 
 	/* 연관관계 설정 */
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "grade_id")
+	private Grade grade;
+
+	@Builder
+	public Grade(String score, Member member, Grade grade) {
+		this.score = score;
+		this.member = member;
+		this.grade = grade;
+	}
 }

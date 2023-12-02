19feb171e7bd03592ebe472f78a44d5b3cbcf332
twg0 <fruitsjh@gmail.com>
2023-12-02 18:13:15 +0900

    feat: MemberRepository Repository 구현

---
+++ src/main/java/com/twg0/myacademy/domain/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.twg0.myacademy.domain.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.twg0.myacademy.domain.member.entity.Member;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}

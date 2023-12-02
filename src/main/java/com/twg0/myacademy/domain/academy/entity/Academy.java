afd7898c21f887286f05800cc50e9a9a1ad33f44
twg0 <fruitsjh@gmail.com>
2023-12-02 18:10:54 +0900

    fix: Academy 연관관계 설정

--- src/main/java/com/twg0/myacademy/domain/academy/entity/Academy.java
+++ src/main/java/com/twg0/myacademy/domain/academy/entity/Academy.java
@@ -1,11 +1,17 @@
 package com.twg0.myacademy.domain.academy.entity;
 
-import com.twg0.myacademy.domain.common.entity.BaseEntity;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.twg0.myacademy.domain.common.entity.BaseEntity;
+import com.twg0.myacademy.domain.member.entity.Member;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,6 +22,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Academy extends BaseEntity {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "academy_id")
 	private Long id;
 
 	private String name;
@@ -32,4 +39,14 @@ public class Academy extends BaseEntity {
 	}
 
 	/* 연관관계 필요 */
+	@OneToMany(mappedBy = "academy")
+	private List<Member> members = new ArrayList<>();
+
+	public void addMembers(Member member) {
+		this.members.add(member);
+	}
+
+	public void removeMembers(Member member) {
+		this.members.remove(member);
+	}
 }

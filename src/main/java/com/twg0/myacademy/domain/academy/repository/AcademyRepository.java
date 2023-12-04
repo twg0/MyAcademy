4d6e5032518adda241619a2b83182d5d7ab24b85
twg0 <fruitsjh@gmail.com>
2023-12-04 23:49:10 +0900

    feat: 학원ID를 기준으로 검색 - findByUserId

--- src/main/java/com/twg0/myacademy/domain/academy/repository/AcademyRepository.java
+++ src/main/java/com/twg0/myacademy/domain/academy/repository/AcademyRepository.java
@@ -1,5 +1,7 @@
 package com.twg0.myacademy.domain.academy.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import com.twg0.myacademy.domain.academy.entity.Academy;
 
 @Repository
 public interface AcademyRepository extends JpaRepository<Academy, Long> {
+
+	Academy findByUserId(final String userId);
 }

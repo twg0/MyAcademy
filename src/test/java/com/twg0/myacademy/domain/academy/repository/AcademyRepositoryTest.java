9d9463481e57ba51858c63c27ddc49dca0913197
twg0 <fruitsjh@gmail.com>
2023-12-04 23:33:00 +0900

    feat: 학원등록 테스트 통과

--- src/test/java/com/twg0/myacademy/domain/academy/repository/AcademyRepositoryTest.java
+++ src/test/java/com/twg0/myacademy/domain/academy/repository/AcademyRepositoryTest.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import com.twg0.myacademy.domain.academy.entity.Academy;
+
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class AcademyRepositoryTest {
@@ -25,12 +27,23 @@ class AcademyRepositoryTest {
 	}
 
 	@Test
-	public void create() throws Exception {
+	public void 학원등록() throws Exception {
 	    // given
+		final Academy academy = Academy.builder()
+			.name("서강학원")
+			.address("서울특별시 송파구 마천동")
+			.phoneNumber("02-123-4567")
+			.studentNumber(200)
+			.build();
 
 	    // when
+		final Academy result = academyRepository.save(academy);
 
 	    // then
-
+		assertThat(result.getId()).isNotNull();
+		assertThat(result.getName()).isEqualTo("서강학원");
+		assertThat(result.getAddress()).isEqualTo("서울특별시 송파구 마천동");
+		assertThat(result.getPhoneNumber()).isEqualTo("02-123-4567");
+		assertThat(result.getStudentNumber()).isEqualTo(200);
 	}
 }

a6136a95256bc250ae4303c10a1c1ab5a5034414
twg0 <fruitsjh@gmail.com>
2023-12-07 21:44:08 +0900

    feat: 조회 메소드 구현 - 멤버 기준 - 반 기준

--- src/main/java/com/twg0/myacademy/domain/classes/repository/MemberClassesRepository.java
+++ src/main/java/com/twg0/myacademy/domain/classes/repository/MemberClassesRepository.java
@@ -1,10 +1,18 @@
 package com.twg0.myacademy.domain.classes.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.twg0.myacademy.domain.classes.entity.Classes;
 import com.twg0.myacademy.domain.classes.entity.MemberClasses;
+import com.twg0.myacademy.domain.member.entity.Member;
 
 @Repository
 public interface MemberClassesRepository extends JpaRepository<MemberClasses, Long> {
+
+	List<MemberClasses> findByMember(Member member);
+
+	List<MemberClasses> findByClasses(Classes classes);
 }

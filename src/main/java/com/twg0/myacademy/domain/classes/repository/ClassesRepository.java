88f8c44fb5c971d2a39cd67467a9e2c3881377c3
twg0 <fruitsjh@gmail.com>
2023-12-07 21:44:39 +0900

    feat: 메소드 구현 - 조회 - 삭제

--- src/main/java/com/twg0/myacademy/domain/classes/repository/ClassesRepository.java
+++ src/main/java/com/twg0/myacademy/domain/classes/repository/ClassesRepository.java
@@ -1,5 +1,9 @@
 package com.twg0.myacademy.domain.classes.repository;
 
+import java.util.Optional;
+
+import javax.swing.text.html.Option;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +11,8 @@ import com.twg0.myacademy.domain.classes.entity.Classes;
 
 @Repository
 public interface ClassesRepository extends JpaRepository<Classes, Long> {
+
+	Optional<Classes> findByClassName(String className);
+
+	void deleteByClassName(String className);
 }

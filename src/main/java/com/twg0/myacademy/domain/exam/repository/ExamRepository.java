37338ef2999ea4b859613b0b4bebb293278538a5
twg0 <fruitsjh@gmail.com>
2023-12-12 00:05:26 +0900

    feat: 시험조회 메소드 구현 - 이름, 날짜

--- src/main/java/com/twg0/myacademy/domain/exam/repository/ExamRepository.java
+++ src/main/java/com/twg0/myacademy/domain/exam/repository/ExamRepository.java
@@ -1,5 +1,8 @@
 package com.twg0.myacademy.domain.exam.repository;
 
+import java.time.Instant;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +10,6 @@ import com.twg0.myacademy.domain.exam.entity.Exam;
 
 @Repository
 public interface ExamRepository extends JpaRepository<Exam, Long> {
+
+	Optional<Exam> findByNameAndDate(String name, Instant Date);
 }

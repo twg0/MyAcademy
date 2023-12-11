0bf8a7f90781b1ef18f7344cdae3971fbece5cdd
twg0 <fruitsjh@gmail.com>
2023-12-12 00:32:42 +0900

    feat: Grade 조회 메소드 구현 - findById - findAllByMember - findAllByExam

--- src/main/java/com/twg0/myacademy/domain/exam/repository/GradeRepository.java
+++ src/main/java/com/twg0/myacademy/domain/exam/repository/GradeRepository.java
@@ -1,10 +1,21 @@
 package com.twg0.myacademy.domain.exam.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.twg0.myacademy.domain.exam.entity.Exam;
 import com.twg0.myacademy.domain.exam.entity.Grade;
+import com.twg0.myacademy.domain.member.entity.Member;
 
 @Repository
 public interface GradeRepository extends JpaRepository<Grade, Long> {
+
+	Optional<Grade> findById(Long id);
+
+	List<Grade> findAllByMember(Member member);
+
+	List<Grade> findAllByExam(Exam exam);
 }

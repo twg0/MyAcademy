bbd36a5983ee71c05e2155b9ddfb5b10b7da8a4a
twg0 <fruitsjh@gmail.com>
2023-12-12 00:37:10 +0900

    test: setUp 설정

--- src/test/java/com/twg0/myacademy/domain/exam/repository/GradeRepositoryTest.java
+++ src/test/java/com/twg0/myacademy/domain/exam/repository/GradeRepositoryTest.java
@@ -1,8 +1,22 @@
 package com.twg0.myacademy.domain.exam.repository;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.twg0.myacademy.domain.academy.entity.Academy;
+import com.twg0.myacademy.domain.academy.repository.AcademyRepository;
+import com.twg0.myacademy.domain.classes.entity.Classes;
+import com.twg0.myacademy.domain.classes.repository.ClassesRepository;
+import com.twg0.myacademy.domain.exam.entity.Exam;
+import com.twg0.myacademy.domain.member.entity.Member;
+import com.twg0.myacademy.domain.member.repository.MemberRepository;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -10,5 +24,65 @@ class GradeRepositoryTest {
 
 	@Autowired
 	private GradeRepository gradeRepository;
+	@Autowired
+	private AcademyRepository academyRepository;
+	@Autowired
+	private MemberRepository memberRepository;
+	@Autowired
+	private ClassesRepository classesRepository;
+	@Autowired
+	private ExamRepository examRepository;
+
+	private Academy ACADEMY;
+	private Member MEMBER;
+	private Instant BIRTH;
+	private Classes CLASSES;
+	private Exam EXAM;
+	private Instant DATE;
+	@BeforeEach
+	public void setUp() {
+		final Academy academy = Academy.builder()
+			.name("서강학원")
+			.address("서울특별시 송파구 마천동")
+			.phoneNumber("02-123-4567")
+			.studentNumber(200)
+			.userId("seokang")
+			.password("tjrkd")
+			.build();
+		ACADEMY = academyRepository.save(academy);
+
+		BIRTH = LocalDateTime.of(1996, 8, 25, 0, 0).atZone(ZoneId.of("Asia/Seoul")).toInstant();
+		DATE = LocalDateTime.of(2023, 11, 11, 15, 30).atZone(ZoneId.of("Asia/Seoul")).toInstant();
+
+		final Member member = Member.builder()
+			.username("홍길동")
+			.userId("hong")
+			.password("gildong")
+			.age(18)
+			.birth(BIRTH)
+			.school("방산")
+			.academy(ACADEMY)
+			.build();
+
+		final Classes classes = Classes.builder()
+			.subject("수학")
+			.className("예비고1A")
+			.countOfStudent(0)
+			.teacher("kim")
+			.academy(ACADEMY)
+			.build();
+
+		MEMBER = memberRepository.save(member);
+		CLASSES = classesRepository.save(classes);
+
+		final Exam exam = Exam.builder()
+			.name("주간테스트")
+			.date(DATE)
+			.countOfStudent(30)
+			.classes(CLASSES)
+			.build();
+
+		EXAM = examRepository.save(exam);
+	}
 
 }

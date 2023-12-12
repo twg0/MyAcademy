c6c3626dd67957540bfacadf81696d0b451350d5
twg0 <fruitsjh@gmail.com>
2023-12-12 21:53:02 +0900

    test: 학원조회테스트 통과

--- src/test/java/com/twg0/myacademy/domain/academy/service/AcademyServiceTest.java
+++ src/test/java/com/twg0/myacademy/domain/academy/service/AcademyServiceTest.java
@@ -18,8 +18,6 @@ import com.twg0.myacademy.domain.academy.repository.AcademyRepository;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class AcademyServiceTest {
 	@Autowired
-	private AcademyRepository academyRepository;
-	@Autowired
 	private AcademyService academyService;
 
 	@Test
@@ -36,6 +34,29 @@ class AcademyServiceTest {
 	    // when
 		AcademyDTO result = academyService.create(academyDTO);
 		// then
+		assertThat(result.getName()).isEqualTo(academyDTO.getName());
+		assertThat(result.getAddress()).isEqualTo(academyDTO.getAddress());
+		assertThat(result.getPhoneNumber()).isEqualTo(academyDTO.getPhoneNumber());
+		assertThat(result.getStudentNumber()).isEqualTo(academyDTO.getStudentNumber());
+		assertThat(result.getUserId()).isEqualTo(academyDTO.getUserId());
+		assertThat(result.getPassword()).isEqualTo(academyDTO.getPassword());
+	}
+
+	@Test
+	public void 학원조회테스트() throws Exception {
+		// given
+		final AcademyDTO academyDTO = AcademyDTO.builder()
+			.name("서강학원")
+			.address("서울특별시 송파구 마천동")
+			.phoneNumber("02-123-4567")
+			.studentNumber(200)
+			.userId("seokang")
+			.password("tjrkd")
+			.build();
+		// when
+		academyService.create(academyDTO);
+		AcademyDTO result = academyService.readByUserId(academyDTO.getUserId());
+	    // then
 		assertThat(result.getName()).isEqualTo(academyDTO.getName());
 		assertThat(result.getAddress()).isEqualTo(academyDTO.getAddress());
 		assertThat(result.getPhoneNumber()).isEqualTo(academyDTO.getPhoneNumber());

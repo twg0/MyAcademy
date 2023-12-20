aaae7834a93e646cf9904a15133d534469e50d2a
twg0 <fruitsjh@gmail.com>
2023-12-20 22:43:32 +0900

    feat: AcademyRequest 구현

---
+++ src/main/java/com/twg0/myacademy/domain/academy/DTO/AcademyRequest.java
@@ -0,0 +1,40 @@
+package com.twg0.myacademy.domain.academy.DTO;
+
+import com.twg0.myacademy.domain.academy.entity.Academy;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class AcademyRequest {
+
+	private String name;
+	private String phoneNumber;
+	private String address;
+	private Integer studentNumber;
+	private String userId;
+	private String password;
+
+	@Builder
+	public AcademyRequest(String name, String phoneNumber, String address, Integer studentNumber, String userId,
+		String password) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+		this.studentNumber = studentNumber;
+		this.userId = userId;
+		this.password = password;
+	}
+
+	public Academy toEntity() {
+		return Academy.builder()
+			.name(this.getName())
+			.phoneNumber(this.getPhoneNumber())
+			.address(this.getAddress())
+			.studentNumber(this.getStudentNumber())
+			.userId(this.getUserId())
+			.password(this.getPassword())
+			.build();
+	}
+}

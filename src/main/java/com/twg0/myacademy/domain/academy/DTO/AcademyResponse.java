57e59fa2eec90807e19c1ecad14ddb48d9996175
twg0 <fruitsjh@gmail.com>
2023-12-20 22:44:00 +0900

    fix: AcademyDTO를 AcademyResponse로 변경 및 수정

--- src/main/java/com/twg0/myacademy/domain/academy/DTO/AcademyResponse.java
+++ src/main/java/com/twg0/myacademy/domain/academy/DTO/AcademyResponse.java
@@ -8,46 +8,31 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class AcademyDTO {
+public class AcademyResponse {
 
 	private String name;
 	private String phoneNumber;
 	private String address;
 	private Integer studentNumber;
 	private String userId;
-	private String password;
 
 	@Builder
-	public AcademyDTO(String name, String phoneNumber, String address, Integer studentNumber, String userId,
-		String password) {
+	public AcademyResponse(String name, String phoneNumber, String address, Integer studentNumber, String userId) {
 		this.name = name;
 		this.phoneNumber = phoneNumber;
 		this.address = address;
 		this.studentNumber = studentNumber;
 		this.userId = userId;
-		this.password = password;
 	}
 
 
-	public static AcademyDTO fromEntity(Academy academy) {
-		return AcademyDTO.builder()
+	public static AcademyResponse fromEntity(Academy academy) {
+		return AcademyResponse.builder()
 			.name(academy.getName())
 			.phoneNumber(academy.getPhoneNumber())
 			.address(academy.getAddress())
 			.studentNumber(academy.getStudentNumber())
 			.userId(academy.getUserId())
-			.password(academy.getPassword())
-			.build();
-	}
-
-	public Academy toEntity() {
-		return Academy.builder()
-			.name(this.getName())
-			.phoneNumber(this.getPhoneNumber())
-			.address(this.getAddress())
-			.studentNumber(this.getStudentNumber())
-			.userId(this.getUserId())
-			.password(this.getPassword())
 			.build();
 	}
 }

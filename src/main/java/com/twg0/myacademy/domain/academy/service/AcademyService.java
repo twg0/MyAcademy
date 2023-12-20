bf801abaaf81184cf6c0f3ca447b2c7199d82cc1
twg0 <fruitsjh@gmail.com>
2023-12-20 22:52:18 +0900

    feat: Academy 관련 기본 CRUD 구현

--- src/main/java/com/twg0/myacademy/domain/academy/service/AcademyService.java
+++ src/main/java/com/twg0/myacademy/domain/academy/service/AcademyService.java
@@ -3,14 +3,13 @@ package com.twg0.myacademy.domain.academy.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.twg0.myacademy.domain.academy.DTO.AcademyDTO;
+import com.twg0.myacademy.domain.academy.DTO.AcademyRequest;
+import com.twg0.myacademy.domain.academy.DTO.AcademyResponse;
 import com.twg0.myacademy.domain.academy.entity.Academy;
 import com.twg0.myacademy.domain.academy.repository.AcademyRepository;
-import com.twg0.myacademy.domain.member.entity.Member;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,19 +23,35 @@ public class AcademyService {
 	private final AcademyRepository academyRepository;
 
 	@Transactional
-	public AcademyDTO create(AcademyDTO academyDTO) {
-		log.info("create 진입");
-		Academy save = academyRepository.save(academyDTO.toEntity());
-		return AcademyDTO.fromEntity(save);
+	public AcademyResponse create(AcademyRequest academyRequest) {
+		// 중복 검사
+		if(academyRepository.existsByUserId(academyRequest.getUserId()))
+			throw new IllegalArgumentException("ID가 이미 존재합니다.");
+
+		Academy save = academyRepository.save(academyRequest.toEntity());
+		return AcademyResponse.fromEntity(save);
 	}
 
-	public AcademyDTO readByUserId(String userId) {
+	@Transactional
+	public AcademyResponse updateInfo(String userId, AcademyRequest academyRequest) {
 		Optional<Academy> result = academyRepository.findByUserId(userId);
-		return AcademyDTO.fromEntity(result.get());
+		return result.get().updateInfo(academyRequest);
 	}
 
-	public List<AcademyDTO> readAll() {
+	@Transactional
+	public void delete(String userId) throws IllegalAccessException {
+		if(!academyRepository.existsByUserId(userId))
+			throw new IllegalAccessException("ID가 존재하지 않습니다.");
+		academyRepository.deleteByUserId(userId);
+	}
+
+	public AcademyResponse read(String userId) {
+		Optional<Academy> result = academyRepository.findByUserId(userId);
+		return AcademyResponse.fromEntity(result.get());
+	}
+
+	public List<AcademyResponse> readAll() {
 		List<Academy> all = academyRepository.findAll();
-		return all.stream().map(AcademyDTO::fromEntity).toList();
+		return all.stream().map(AcademyResponse::fromEntity).toList();
 	}
 }

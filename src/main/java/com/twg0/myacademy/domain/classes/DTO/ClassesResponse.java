5faae0ed5e8e4806f3384b2916143264f85117bc
twg0 <fruitsjh@gmail.com>
2023-12-21 22:22:43 +0900

    feat: ClassesResponse 구현

---
+++ src/main/java/com/twg0/myacademy/domain/classes/DTO/ClassesResponse.java
@@ -0,0 +1,32 @@
+package com.twg0.myacademy.domain.classes.DTO;
+
+import com.twg0.myacademy.domain.classes.entity.Classes;
+
+import jakarta.persistence.Column;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class ClassesResponse {
+	private String className;
+	private String subject;
+	private String teacher;
+	private Integer countOfStudent;
+
+	@Builder
+	public ClassesResponse(String className, String subject, String teacher, Integer countOfStudent) {
+		this.className = className;
+		this.subject = subject;
+		this.teacher = teacher;
+		this.countOfStudent = countOfStudent;
+	}
+
+	public static ClassesResponse fromEntity(Classes classes) {
+		return ClassesResponse.builder()
+			.className(classes.getClassName())
+			.subject(classes.getSubject())
+			.teacher(classes.getTeacher())
+			.countOfStudent(classes.getCountOfStudent())
+			.build();
+	}
+}

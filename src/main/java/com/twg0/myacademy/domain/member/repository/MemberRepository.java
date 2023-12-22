321f2d936cf8c186d49e16a33403823bd4ccecc7
twg0 <fruitsjh@gmail.com>
2023-12-22 13:58:01 +0900

    feat: userId 존재 확인 메소드 구현

--- src/main/java/com/twg0/myacademy/domain/member/repository/MemberRepository.java
+++ src/main/java/com/twg0/myacademy/domain/member/repository/MemberRepository.java
@@ -12,7 +12,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	Optional<Member> findByUserId(String userId);
 
-	boolean existsBy(String userId);
+	boolean existsByUserId(String userId);
 
 	void deleteByUserId(String userId);
 }

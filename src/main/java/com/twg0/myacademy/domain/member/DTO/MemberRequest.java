4257030ec2788eaeb236b6fec569b8a40284457b
twg0 <fruitsjh@gmail.com>
2023-12-22 14:16:20 +0900

    feat: MemberRequest toEntity 메서드 age 필드 추가

--- src/main/java/com/twg0/myacademy/domain/member/DTO/MemberRequest.java
+++ src/main/java/com/twg0/myacademy/domain/member/DTO/MemberRequest.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 
 import com.twg0.myacademy.domain.academy.entity.Academy;
 import com.twg0.myacademy.domain.member.entity.Member;
+import com.twg0.myacademy.domain.member.enums.Role;
 
 import lombok.Builder;
 import lombok.Data;
@@ -17,18 +18,21 @@ public class MemberRequest {
 	private LocalDateTime birth;
 	private String school;
 	private Integer age;
+	private Role role;
 	private Academy academy;
 
 	@Builder
 	public MemberRequest(String userId, String username, String password, LocalDateTime birth, String school,
 		Integer age,
-		Academy academy) {
+		Role role, Academy academy) {
 		this.userId = userId;
 		this.username = username;
 		this.password = password;
 		this.birth = birth;
 		this.school = school;
 		this.age = age;
+		this.role = role;
+		this.academy = academy;
 	}
 
 	public Member toEntity() {
@@ -36,9 +40,11 @@ public class MemberRequest {
 			.userId(this.userId)
 			.username(this.username)
 			.password(this.password)
+			.age(this.age)
 			.birth(this.birth)
 			.school(this.school)
 			.academy(this.academy)
+			.role(this.role)
 			.build();
 	}
 }

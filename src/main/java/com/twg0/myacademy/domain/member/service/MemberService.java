6927c990582df8d7d710f63b193900f8f37c5b90
twg0 <fruitsjh@gmail.com>
2023-12-22 14:18:31 +0900

    feat: create 메서드 구현

---
+++ src/main/java/com/twg0/myacademy/domain/member/service/MemberService.java
@@ -0,0 +1,35 @@
+package com.twg0.myacademy.domain.member.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.twg0.myacademy.domain.academy.entity.Academy;
+import com.twg0.myacademy.domain.academy.repository.AcademyRepository;
+import com.twg0.myacademy.domain.member.DTO.MemberRequest;
+import com.twg0.myacademy.domain.member.DTO.MemberResponse;
+import com.twg0.myacademy.domain.member.entity.Member;
+import com.twg0.myacademy.domain.member.enums.Role;
+import com.twg0.myacademy.domain.member.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+	private final MemberRepository memberRepository;
+	private final AcademyRepository academyRepository;
+
+	@Transactional
+	public MemberResponse create(MemberRequest memberRequest, String academyUserId) {
+		if(memberRepository.existsByUserId(memberRequest.getUserId())) {
+			throw new IllegalArgumentException("ID가 이미 존재합니다.");
+		}
+		Academy academy = academyRepository.findByUserId(academyUserId).get();
+		memberRequest.setAcademy(academy);
+		Member member = memberRepository.save(memberRequest.toEntity());
+		return MemberResponse.fromEntity(member);
+	}
+}

67c4c28677a5236d952709f1ed63144196bb82f2
twg0 <fruitsjh@gmail.com>
2023-12-23 22:41:13 +0900

    test: 수강등록 메서드 테스트 통과

---
+++ src/test/java/com/twg0/myacademy/domain/classes/service/MemberClassesServiceTest.java
@@ -0,0 +1,90 @@
+package com.twg0.myacademy.domain.classes.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.twg0.myacademy.domain.academy.entity.Academy;
+import com.twg0.myacademy.domain.academy.repository.AcademyRepository;
+import com.twg0.myacademy.domain.classes.DTO.MemberClassesDTO;
+import com.twg0.myacademy.domain.classes.entity.Classes;
+import com.twg0.myacademy.domain.classes.repository.ClassesRepository;
+import com.twg0.myacademy.domain.member.entity.Member;
+import com.twg0.myacademy.domain.member.enums.Role;
+import com.twg0.myacademy.domain.member.repository.MemberRepository;
+
+@SpringBootTest
+@Transactional
+class MemberClassesServiceTest {
+
+	@Autowired
+	private MemberClassesService memberClassesService;
+	@Autowired
+	private AcademyRepository academyRepository;
+	@Autowired
+	private MemberRepository memberRepository;
+	@Autowired
+	private ClassesRepository classesRepository;
+
+	private Academy ACADEMY;
+	private Member MEMBER;
+	private LocalDateTime BIRTH;
+	private Classes CLASSES;
+	@BeforeEach
+	public void setUp() {
+		final Academy academy = Academy.builder()
+			.name("서강학원")
+			.address("서울특별시 송파구 마천동")
+			.phoneNumber("02-123-4567")
+			.studentNumber(200)
+			.userId("seokang")
+			.password("tjrkd")
+			.build();
+		ACADEMY = academyRepository.save(academy);
+
+		BIRTH = LocalDateTime.of(1996, 8, 25, 0, 0).atZone(ZoneId.of("Asia/Seoul")).toLocalDateTime();
+
+		final Member member = Member.builder()
+			.username("홍길동")
+			.userId("hong")
+			.password("gildong")
+			.age(18)
+			.birth(BIRTH)
+			.school("방산")
+			.role(Role.MEMBER)
+			.academy(ACADEMY)
+			.build();
+
+		final Classes classes = Classes.builder()
+			.subject("수학")
+			.className("예비고1A")
+			.countOfStudent(0)
+			.teacher("kim")
+			.academy(ACADEMY)
+			.build();
+
+		MEMBER = memberRepository.save(member);
+		CLASSES = classesRepository.save(classes);
+	}
+
+	@Test
+	public void 수강등록() throws Exception {
+	    // given
+		String userId = MEMBER.getUserId();
+		String className = CLASSES.getClassName();
+		// when
+		MemberClassesDTO result = memberClassesService.create(userId, className);
+		// then
+		assertThat(result.getMember()).isEqualTo(MEMBER);
+		assertThat(result.getClasses()).isEqualTo(CLASSES);
+	}
+}

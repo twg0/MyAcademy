b522f2f19ee9144321ff4b0c1f3f12b02620005c
twg0 <fruitsjh@gmail.com>
2023-12-24 20:30:14 +0900

    test: 시험등록 테스트 통과

---
+++ src/test/java/com/twg0/myacademy/domain/exam/service/ExamServiceTest.java
@@ -0,0 +1,78 @@
+package com.twg0.myacademy.domain.exam.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.twg0.myacademy.domain.academy.entity.Academy;
+import com.twg0.myacademy.domain.academy.repository.AcademyRepository;
+import com.twg0.myacademy.domain.classes.entity.Classes;
+import com.twg0.myacademy.domain.classes.repository.ClassesRepository;
+import com.twg0.myacademy.domain.exam.DTO.ExamRequest;
+import com.twg0.myacademy.domain.exam.DTO.ExamResponse;
+
+@SpringBootTest
+@Transactional
+class ExamServiceTest {
+	@Autowired
+	private ExamService examService;
+	@Autowired
+	private AcademyRepository academyRepository;
+	@Autowired
+	private ClassesRepository classesRepository;
+
+	private Academy ACADEMY;
+	private Classes CLASSES;
+	private LocalDateTime DATE;
+
+	@BeforeEach
+	public void setUp() {
+		final Academy academy = Academy.builder()
+			.name("서강학원")
+			.address("서울특별시 송파구 마천동")
+			.phoneNumber("02-123-4567")
+			.studentNumber(200)
+			.userId("seokang")
+			.password("tjrkd")
+			.build();
+		ACADEMY = academyRepository.save(academy);
+
+		DATE = LocalDateTime.of(2023, 11, 11, 15, 30).atZone(ZoneId.of("Asia/Seoul")).toLocalDateTime();
+
+		final Classes classes = Classes.builder()
+			.subject("수학")
+			.className("예비고1A")
+			.countOfStudent(0)
+			.teacher("kim")
+			.academy(ACADEMY)
+			.build();
+
+		CLASSES = classesRepository.save(classes);
+	}
+
+	@Test
+	public void 시험등록() throws Exception {
+	    // given
+		final ExamRequest examRequest = ExamRequest.builder()
+			.date(DATE)
+			.name("주간테스트")
+			.countOfStudent(10)
+			.build();
+	    // when
+		ExamResponse result = examService.create(CLASSES.getClassName(), examRequest);
+		// then
+		assertThat(result.getName()).isEqualTo(examRequest.getName());
+		assertThat(result.getDate()).isEqualTo(examRequest.getDate());
+		assertThat(result.getCountOfStudent()).isEqualTo(examRequest.getCountOfStudent());
+		assertThat(result.getClasses()).isEqualTo(examRequest.getClasses());
+	}
+}

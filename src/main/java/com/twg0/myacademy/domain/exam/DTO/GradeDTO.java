63d962eacd25a89d640640a42246d83c94145bdf
twg0 <fruitsjh@gmail.com>
2023-12-26 23:58:01 +0900

    feat: GradeDTO 구현

---
+++ src/main/java/com/twg0/myacademy/domain/exam/DTO/GradeDTO.java
@@ -0,0 +1,28 @@
+package com.twg0.myacademy.domain.exam.DTO;
+
+import com.twg0.myacademy.domain.exam.entity.Exam;
+import com.twg0.myacademy.domain.exam.entity.Grade;
+import com.twg0.myacademy.domain.member.entity.Member;
+
+import jakarta.persistence.Lob;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class GradeDTO {
+	private String memberExam;
+	private String score;
+
+	@Builder
+	public GradeDTO(String memberExam, String score) {
+		this.memberExam = memberExam;
+		this.score = score;
+	}
+
+	public static GradeDTO fromEntity(Grade grade) {
+		return GradeDTO.builder()
+			.memberExam(grade.getMemberExam())
+			.score(grade.getScore())
+			.build();
+	}
+}

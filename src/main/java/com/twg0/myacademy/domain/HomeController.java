47db119c03f2cfc97650f816fa3f23603307a383
twg0 <fruitsjh@gmail.com>
2023-12-28 14:32:55 +0900

    feat: HomeController 구현

---
+++ src/main/java/com/twg0/myacademy/domain/HomeController.java
@@ -0,0 +1,14 @@
+package com.twg0.myacademy.domain;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+}

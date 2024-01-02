334dcb1edaa64bfc9b401484676a66ab96d4346b
twg0 <fruitsjh@gmail.com>
2024-01-02 15:21:49 +0900

    feat: 학원정보수정 API 구현

--- src/main/java/com/twg0/myacademy/domain/academy/controller/AcademyController.java
+++ src/main/java/com/twg0/myacademy/domain/academy/controller/AcademyController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,5 +37,14 @@ public class AcademyController {
 	) {
 		AcademyResponse academyResponse = academyService.create(academyRequest);
 		return new ResponseEntity<>(academyResponse, HttpStatus.CREATED);
+	}
+
+	@PatchMapping("/{academyUserId}")
+	public ResponseEntity<AcademyResponse> update(
+		@PathVariable("academyUserId") String academyUserId,
+		@RequestBody AcademyRequest academyRequest
+	) {
+		AcademyResponse academyResponse = academyService.updateInfo(academyUserId, academyRequest);
+		return new ResponseEntity<>(academyResponse, HttpStatus.OK);
 	}
 }

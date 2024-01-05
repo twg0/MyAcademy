ebec8cd3127c8ea71091e48b32ba5d97df8e6d4a
twg0 <fruitsjh@gmail.com>
2024-01-05 14:06:24 +0900

    feat: 최상위 예외 클래스 구현

---
+++ src/main/java/com/twg0/myacademy/domain/common/exception/BusinessException.java
@@ -0,0 +1,16 @@
+package com.twg0.myacademy.domain.common.exception;
+
+public class BusinessException extends RuntimeException{
+	private final ErrorCode errorCode;
+	private final String detail;
+
+	public BusinessException(ErrorCode errorCode, String detail) {
+		this.errorCode = errorCode;
+		this.detail = detail;
+	}
+
+	public BusinessException(ErrorCode errorCode) {
+		this.errorCode = errorCode;
+		this.detail = null;
+	}
+}

cc1433af25520b69b2f4f8061c4dd4d71cc6f574
twg0 <fruitsjh@gmail.com>
2024-01-05 14:46:03 +0900

    feat: 반등록API 구현

--- src/main/java/com/twg0/myacademy/domain/classes/controller/ClassesController.java
+++ src/main/java/com/twg0/myacademy/domain/classes/controller/ClassesController.java
@@ -4,13 +4,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.twg0.myacademy.domain.academy.DTO.AcademyResponse;
 import com.twg0.myacademy.domain.academy.service.AcademyService;
+import com.twg0.myacademy.domain.classes.DTO.ClassesRequest;
 import com.twg0.myacademy.domain.classes.DTO.ClassesResponse;
 import com.twg0.myacademy.domain.classes.service.ClassesService;
+import com.twg0.myacademy.domain.common.exception.ErrorCode;
+import com.twg0.myacademy.domain.common.exception.entitynotfound.AcademyNotFoundException;
+import com.twg0.myacademy.domain.common.exception.entitynotfound.ClassesNotFoundException;
+import com.twg0.myacademy.domain.common.exception.invalidvalue.DuplicatedException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,13 +33,38 @@ public class ClassesController {
 		@PathVariable("academyUserId") String academyUserId,
 		@PathVariable("className") String className
 	) {
-		if (!academyService.exist(academyUserId)) {
-			throw new IllegalArgumentException("존재하지 않는 학원입니다.");
+		if (!isAcademyExist(academyUserId)) {
+			throw new AcademyNotFoundException(ErrorCode.ACADEMY_NOT_FOUND);
 		}
-		if (!classesService.existByClassName(className)) {
-			throw new IllegalArgumentException("존재하지 않는 반입니다.");
+		if (!isClassesExist(className)) {
+			throw new ClassesNotFoundException(ErrorCode.CLASSES_NOT_FOUND);
 		}
+
 		ClassesResponse classesResponse = classesService.read(className);
 		return new ResponseEntity<>(classesResponse, HttpStatus.FOUND);
+	}
+
+	@PostMapping
+	public ResponseEntity<ClassesResponse> create(
+		@PathVariable("academyUserId") String academyUserId,
+		@RequestBody ClassesRequest classesRequest
+	) {
+		if (!isAcademyExist(academyUserId)) {
+			throw new AcademyNotFoundException(ErrorCode.ACADEMY_NOT_FOUND);
+		}
+		if (isClassesExist(classesRequest.getClassName())) {
+			throw new DuplicatedException(ErrorCode.ID_DUPLICATED);
+		}
+
+		ClassesResponse classesResponse = classesService.create(classesRequest, academyUserId);
+		return new ResponseEntity<>(classesResponse, HttpStatus.CREATED);
+	}
+
+	private boolean isAcademyExist(String academyUserId) {
+		return academyService.exist(academyUserId);
+	}
+
+	private boolean isClassesExist(String className) {
+		return classesService.existByClassName(className);
 	}
 }

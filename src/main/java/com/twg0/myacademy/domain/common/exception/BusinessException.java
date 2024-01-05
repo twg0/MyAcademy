af3ea0e342f6dc5d14630c33118a3390d66a92f3
twg0 <fruitsjh@gmail.com>
2024-01-05 15:31:20 +0900

    feat: Getter 설정

--- src/main/java/com/twg0/myacademy/domain/common/exception/BusinessException.java
+++ src/main/java/com/twg0/myacademy/domain/common/exception/BusinessException.java
@@ -1,5 +1,8 @@
 package com.twg0.myacademy.domain.common.exception;
 
+import lombok.Getter;
+
+@Getter
 public class BusinessException extends RuntimeException{
 	private final ErrorCode errorCode;
 	private final String detail;

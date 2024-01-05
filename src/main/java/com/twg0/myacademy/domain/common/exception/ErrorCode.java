8341cf31e58a73ee388f513971fa127ee7b3db70
twg0 <fruitsjh@gmail.com>
2024-01-05 14:19:04 +0900

    feat: not found 에러코드 작성 - Academy - Classes - Member - Exam

--- src/main/java/com/twg0/myacademy/domain/common/exception/ErrorCode.java
+++ src/main/java/com/twg0/myacademy/domain/common/exception/ErrorCode.java
@@ -6,15 +6,15 @@ import lombok.Getter;
 public enum ErrorCode {
 
 	// Academy
-
+	ACADEMY_NOT_FOUND("ACADEMY_001", "해당 학원을 찾을 수 없습니다.", 404),
 	// Classes
-
+	CLASSES_NOT_FOUND("CLASSES_001", "해당 반을 찾을 수 없습니다.", 404),
 	// Member
-
+	MEMBER_NOT_FOUND("MEMBER_001", "해당 사용자를 찾을 수 없습니다.", 404),
 	// MemberClasses
 
 	// Exam
-
+	EXAM_NOT_FOUND("EXAM_001", "해당 시험을 찾을 수 없습니다.", 404),
 	// Grade
 
 

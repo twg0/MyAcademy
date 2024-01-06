3b6d4e402ab94b71da7d9bb67229c622dd861291
twg0 <fruitsjh@gmail.com>
2024-01-06 18:14:08 +0900

    feat: 학원사용자등록API 메소드 구현

--- src/main/java/com/twg0/myacademy/domain/member/controller/MemberController.java
+++ src/main/java/com/twg0/myacademy/domain/member/controller/MemberController.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +13,10 @@ import com.twg0.myacademy.domain.academy.service.AcademyService;
 import com.twg0.myacademy.domain.common.exception.ErrorCode;
 import com.twg0.myacademy.domain.common.exception.entitynotfound.AcademyNotFoundException;
 import com.twg0.myacademy.domain.common.exception.entitynotfound.MemberNotFoundException;
+import com.twg0.myacademy.domain.common.exception.invalidvalue.DuplicatedException;
+import com.twg0.myacademy.domain.member.DTO.MemberRequest;
 import com.twg0.myacademy.domain.member.DTO.MemberResponse;
+import com.twg0.myacademy.domain.member.enums.Role;
 import com.twg0.myacademy.domain.member.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
@@ -43,5 +48,19 @@ public class MemberController {
 
 	private boolean isMemberExist(String memberUserId) {
 		return memberService.exist(memberUserId);
+	}
+
+	@PostMapping
+	public ResponseEntity<MemberResponse> create(
+		@PathVariable("academyUserId") String academyUserId,
+		@RequestBody MemberRequest memberRequest
+	) {
+		if(!isAcademyExist(academyUserId))
+			throw new AcademyNotFoundException(ErrorCode.ACADEMY_NOT_FOUND);
+		if(isMemberExist(memberRequest.getUserId()))
+			throw new DuplicatedException(ErrorCode.ID_DUPLICATED);
+
+		MemberResponse response = memberService.create(memberRequest, academyUserId, Role.MEMBER);
+		return new ResponseEntity<>(response, HttpStatus.CREATED);
 	}
 }

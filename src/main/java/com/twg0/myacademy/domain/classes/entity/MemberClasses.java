fc665f077a4d1c1d1e0f3908d101258d37ea1fae
twg0 <fruitsjh@gmail.com>
2024-01-07 18:49:48 +0900

    feat: 생성 메소드 구현

--- src/main/java/com/twg0/myacademy/domain/classes/entity/MemberClasses.java
+++ src/main/java/com/twg0/myacademy/domain/classes/entity/MemberClasses.java
@@ -8,55 +8,46 @@ import com.twg0.myacademy.domain.member.entity.Member;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
+@EqualsAndHashCode
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberClasses extends BaseEntity {
 
-	@EmbeddedId
-	private MemberClassesID memberClassesID = new MemberClassesID();
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 	/* 연관관계 설정 */
 	@ManyToOne(fetch = FetchType.LAZY)
-	@MapsId("memberId")
 	@JoinColumn(name = "member_id")
 	private Member member;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@MapsId("classesId")
 	@JoinColumn(name = "classes_id")
 	private Classes classes;
 
-	@Builder
-	public MemberClasses(Member member, Classes classes) {
+	protected MemberClasses(Member member, Classes classes) {
 		this.member = member;
-		member.addMemberClasses(this);
 		this.classes = classes;
-		classes.addMemberClasses(this);
 	}
 
-	/* Override */
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		MemberClasses that = (MemberClasses)o;
-		return member.equals(that.member) && classes.equals(that.classes);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(member, classes);
+	public static MemberClasses createMemberClasses(Member member, Classes classes) {
+		MemberClasses memberClasses = new MemberClasses(member, classes);
+		member.addMemberClasses(memberClasses);
+		classes.addMemberClasses(memberClasses);
+		return memberClasses;
 	}
 }

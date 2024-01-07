10c365162b8a2817980546d0a12bef8bf30a7b6c
twg0 <fruitsjh@gmail.com>
2024-01-07 23:39:11 +0900

    feat: 주석 추가 및 반학생등록 메소드 구현

--- src/main/java/com/twg0/myacademy/domain/classes/service/ClassesService.java
+++ src/main/java/com/twg0/myacademy/domain/classes/service/ClassesService.java
@@ -10,7 +10,10 @@ import com.twg0.myacademy.domain.academy.repository.AcademyRepository;
 import com.twg0.myacademy.domain.classes.DTO.ClassesRequest;
 import com.twg0.myacademy.domain.classes.DTO.ClassesResponse;
 import com.twg0.myacademy.domain.classes.entity.Classes;
+import com.twg0.myacademy.domain.classes.entity.MemberClasses;
 import com.twg0.myacademy.domain.classes.repository.ClassesRepository;
+import com.twg0.myacademy.domain.member.entity.Member;
+import com.twg0.myacademy.domain.member.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,7 +25,14 @@ import lombok.extern.slf4j.Slf4j;
 public class ClassesService {
 	private final ClassesRepository classesRepository;
 	private final AcademyRepository academyRepository;
+	private final MemberRepository memberRepository;
 
+	/**
+	 * 반 생성
+	 * @param classesRequest
+	 * @param academyUserId
+	 * @return
+	 */
 	@Transactional
 	public ClassesResponse create(ClassesRequest classesRequest, String academyUserId) {
 		Academy academy = academyRepository.findByAcademyUserId(academyUserId).get();
@@ -32,6 +42,12 @@ public class ClassesService {
 		return ClassesResponse.fromEntity(classes);
 	}
 
+	/**
+	 * 반 수정
+	 * @param className
+	 * @param classesRequest
+	 * @return
+	 */
 	@Transactional
 	public ClassesResponse updateInfo(String className, ClassesRequest classesRequest) {
 		Classes classes = classesRepository.findByClassName(className).get();
@@ -39,6 +55,10 @@ public class ClassesService {
 		return ClassesResponse.fromEntity(classes);
 	}
 
+	/**
+	 * 반 삭제
+	 * @param className
+	 */
 	@Transactional
 	public void delete(String className) {
 		Classes classes = classesRepository.findByClassName(className).get();
@@ -46,14 +66,31 @@ public class ClassesService {
 		classesRepository.deleteByClassName(className);
 	}
 
+	/**
+	 * 반 정보 읽기
+	 * @param className
+	 * @return
+	 */
 	public ClassesResponse read(String className) {
 		Classes classes = classesRepository.findByClassName(className).get();
 		return ClassesResponse.fromEntity(classes);
 	}
 
-	public List<ClassesResponse> readAll(String className) {
-		List<Classes> classesList = classesRepository.findAllByClassName(className);
+	/**
+	 * 전체 반 정보 읽기
+	 * @return
+	 */
+	public List<ClassesResponse> readAll() {
+		List<Classes> classesList = classesRepository.findAll();
 		return classesList.stream().map(ClassesResponse::fromEntity).toList();
+	}
+
+	@Transactional
+	public ClassesResponse register(String className, String memberUserId) {
+		Classes classes = classesRepository.findByClassName(className).get();
+		Member member = memberRepository.findByUserId(memberUserId).get();
+		MemberClasses.createMemberClasses(member, classes);
+		return ClassesResponse.fromEntity(classes);
 	}
 
 	public boolean existByClassName(String className) {

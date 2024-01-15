c8a02ca1829004d1adf2333c121306d50d5d9b9c
twg0 <fruitsjh@gmail.com>
2024-01-16 00:03:35 +0900

    feat: JPAQueryFactory 메소드 추가

--- src/main/java/com/twg0/myacademy/MyAcademyApplication.java
+++ src/main/java/com/twg0/myacademy/MyAcademyApplication.java
@@ -1,15 +1,46 @@
 package com.twg0.myacademy;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.twg0.myacademy.domain.academy.DTO.AcademyRequest;
+import com.twg0.myacademy.domain.academy.entity.Academy;
+import com.twg0.myacademy.domain.academy.service.AcademyService;
+import com.twg0.myacademy.domain.classes.DTO.ClassesRequest;
+import com.twg0.myacademy.domain.classes.service.ClassesService;
+import com.twg0.myacademy.domain.member.DTO.MemberRequest;
+import com.twg0.myacademy.domain.member.enums.Role;
+import com.twg0.myacademy.domain.member.service.MemberService;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@RequiredArgsConstructor
 public class MyAcademyApplication {
+	private final EntityManager entityManager;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyAcademyApplication.class, args);
 	}
-
+	@Bean
+	public JPAQueryFactory jpaQueryFactory() {
+		return new JPAQueryFactory(entityManager);
+	}
 }

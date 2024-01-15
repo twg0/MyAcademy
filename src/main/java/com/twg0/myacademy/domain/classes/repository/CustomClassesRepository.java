f4d06f4c75fc3394aad59d1a13adbfaadee4b3c3
twg0 <fruitsjh@gmail.com>
2024-01-16 00:00:11 +0900

    feat: CustomClassesRepository 구현

---
+++ src/main/java/com/twg0/myacademy/domain/classes/repository/CustomClassesRepository.java
@@ -0,0 +1,9 @@
+package com.twg0.myacademy.domain.classes.repository;
+
+import java.util.List;
+
+import com.twg0.myacademy.domain.classes.entity.Classes;
+
+public interface CustomClassesRepository {
+	// List<Classes> findAllByMemberUserId(String memberUserId);
+}

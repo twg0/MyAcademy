aa976be58271636cc3fbdef007b52abd861e0c17
twg0 <fruitsjh@gmail.com>
2024-01-16 00:03:48 +0900

    feat: MainHome.vue 예시 구현

---
+++ MyAcademyUI/src/components/MainHome.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="visual">
+        <div class="inner">
+
+        </div>
+    </section>
+
+</template>
+
+<script>
+export default {
+    name: "MainHome"
+}
+</script>
+
+<style scoped>
+.visual {
+    width: 1100px;
+    position: relative;
+    background-image: url("../assets/home_blackboard.jpg");
+    margin: 0 auto;
+}
+
+.visual .inner {
+    height: 791px;
+    margin: 0 auto;
+}
+</style>

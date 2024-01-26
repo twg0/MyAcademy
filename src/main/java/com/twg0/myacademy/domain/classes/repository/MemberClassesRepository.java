f56415e538e1edb460ae5f72edbae5b9be50e836
twg0 <fruitsjh@gmail.com>
2024-01-26 13:43:07 +0900

    feat: MemberClassesRepository find 메소드 구현

--- src/main/java/com/twg0/myacademy/domain/classes/repository/MemberClassesRepository.java
+++ src/main/java/com/twg0/myacademy/domain/classes/repository/MemberClassesRepository.java
@@ -1,10 +1,16 @@
 package com.twg0.myacademy.domain.classes.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.twg0.myacademy.domain.classes.entity.Classes;
 import com.twg0.myacademy.domain.classes.entity.MemberClasses;
+import com.twg0.myacademy.domain.member.entity.Member;
 
 @Repository
 public interface MemberClassesRepository extends JpaRepository<MemberClasses, Long> {
+
+	Optional<MemberClasses> findByMemberAndClassesAndAcademyUserId(Member member, Classes classes, String academyUserId);
 }
